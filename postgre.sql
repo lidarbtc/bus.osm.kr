132f3ff10fae7bc62a2611ee95b9e0b863ac5fba
lidar <lidarbtc@protonmail.com>
2024-06-19 20:34:00 +0900

    Add vercel to gitignore

--- postgre.sql
+++ postgre.sql
@@ -1,21 +1,21 @@
 CREATE TABLE ggd_bus_stops (
-    시군명 VARCHAR(50),
-    정류소명 VARCHAR(100),
-    정류소영문명 VARCHAR(100),
+    시군명 TEXT,
+    정류소명 TEXT,
+    정류소영문명 TEXT,
     정류소id INTEGER,
     정류소번호 FLOAT,
-    중앙차로여부 VARCHAR(50),
-    관할관청 VARCHAR(100),
-    위치 VARCHAR(100),
+    중앙차로여부 TEXT,
+    관할관청 TEXT,
+    위치 TEXT,
     WGS84위도 FLOAT,
     WGS84경도 FLOAT
 );
 
 CREATE TABLE seoul_bus_stops (
     NODE_ID INTEGER,
-    ARS_ID VARCHAR(10),
-    정류소명 VARCHAR(100),
+    ARS_ID TEXT,
+    정류소명 TEXT,
     X좌표 FLOAT,
     Y좌표 FLOAT,
-    정류소타입 VARCHAR(50)
+    정류소타입 TEXT
 );
