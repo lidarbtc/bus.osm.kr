c0c45d7b9989515b938d4eb752b21bb1ea15925b
lidarbtc <lidarbtc@protonmail.com>
2025-08-02 11:59:31 +0900

    Fix: truncate to delete

--- script/csv2pg.py
+++ script/csv2pg.py
@@ -1,41 +1,48 @@
-import psycopg2
 import pandas as pd
+import psycopg2
 
-def load_csv_to_postgresql(csv_file, table_name, conn, delimiter=',', line_terminator='\n'):
+
+def load_csv_to_postgresql(
+    csv_file, table_name, conn, delimiter=",", line_terminator="\n"
+):
     # CSV 파일을 DataFrame으로 읽기
     df = pd.read_csv(csv_file, delimiter=delimiter, lineterminator=line_terminator)
-    
+
     # 테이블의 기존 데이터를 삭제
     with conn.cursor() as cursor:
-        cursor.execute(f"TRUNCATE TABLE {table_name} RESTART IDENTITY")
+        cursor.execute(f"DELETE FROM {table_name}")
         conn.commit()
 
         # DataFrame을 PostgreSQL 테이블로 복사
         for i, row in df.iterrows():
-            placeholders = ', '.join(['%s'] * len(row))
-            columns = ', '.join(row.index)
+            placeholders = ", ".join(["%s"] * len(row))
+            columns = ", ".join(row.index)
             sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
             cursor.execute(sql, tuple(row))
         conn.commit()
 
+
 def main():
     # PostgreSQL 데이터베이스에 연결
     conn = psycopg2.connect(
-        host="localhost",    # PostgreSQL 서버 주소
+        host="localhost",  # PostgreSQL 서버 주소
         database="bus",  # 데이터베이스 이름
         user="bus",  # 데이터베이스 사용자
-        password="1234"  # 사용자 비밀번호
+        password="1234",  # 사용자 비밀번호
     )
-    
+
     try:
         # 경기도 버스 정류장 정보 삽입
-        load_csv_to_postgresql('ggd.csv', 'ggd_bus_stops', conn, delimiter='|', line_terminator='^')
-        
+        load_csv_to_postgresql(
+            "ggd.csv", "ggd_bus_stops", conn, delimiter="|", line_terminator="^"
+        )
+
         # 서울 버스 정류장 정보 삽입
-        load_csv_to_postgresql('seoul.csv', 'seoul_bus_stops', conn)
+        load_csv_to_postgresql("seoul.csv", "seoul_bus_stops", conn)
     finally:
         # 연결 종료
         conn.close()
+
 
 if __name__ == "__main__":
     main()
